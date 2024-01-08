5d31c1398d38556d3ffefc3ac98e373d6d02e95d
Chris Ren <ren.chris@outlook.com>
2024-01-08 15:14:14 -0500

    feat: setup park ui

--- panda.config.ts
+++ panda.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
+
   // Where to look for your css declarations
   include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
 
@@ -15,6 +17,8 @@ export default defineConfig({
   theme: {
     extend: {},
   },
+
+  jsxFramework: 'react',
 
   // The output directory for your css system
   outdir: 'styled-system',
